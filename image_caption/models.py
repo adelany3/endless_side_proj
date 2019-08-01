6c93d9a7be2d2b47f60b13cffff295e0d752128a
Alec Delany <Alec.Delany@360i.com>
2019-08-01 10:26:11 -0400

    adding training

---
+++ image_caption/models.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 17:14:09 2019
+
+@author: alec.delany
+"""
+
+import torch
+from torch import nn
+import torchvision
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class Encoder(nn.Module):
+    
+    def __init__(self, encoded_img_size = 14):
+        super (Encoder, self).__init__()
+        self.encoded_img_size = encoded_img_size
+        resnet = torchvision.models.resnet101(pretrained = True)
+        
+        #remove classification layers
+        modules = list(resnet.children())[:-2]
+        self.resnet = nn.Sequential(*modules)
+        
+        self.adpative_pool= nn.AdaptiveAvgPool2d((self.encoded_img_size, self.encoded_img_size))
+        self.fine_tune()
+        
+    def forward(self, images):
+        '''
+        Forward Pass
+        :param images: tenor representing image (batch_size, 3, img, img)
+        :return: encoded image
+        '''
+        out = self.resnet(images)  # (batch_size, 2048, image_size/32, image_size/32)
+        out = self.adaptive_pool(out)  # (batch_size, 2048, encoded_image_size, encoded_image_size)
+        out = out.permute(0, 2, 3, 1)  # (batch_size, encoded_image_size, encoded_image_size, 2048)
+        return out
+    
+    def fine_tune(self, fine_tune = True):
+        '''
+        Fine tune Conv blocks 2, 3, and 4 of resent
+        '''
+        
+        for p in self.resnet.parameters():
+            p.requires_grad = False
+        for child in list(self.resnet.children())[5:]:
+            for p in child.parameters():
+                p.requires_grad = fine_tune
+    
+
+            
+class Attention(nn.Module):
+    
+    def __init__(self, encoder_dim, decoder_dim, attention_dim):
+        '''
+        :param encoder_dim: size of encoded image
+        :param decoder_dim: size of RNN decoder
+        :param attenion_dim: size of attention network
+        '''
+        super(Attention, self).__init__()
+        self.encoder_atten = nn.Linear(encoder_dim, attention_dim)
+        self.decoder_atten = nn.Linear(decoder_dim, attention_dim)
+        self.full_atten = nn.Linear(attention_dim, 1)
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim = 1)
+        
+    def forward(self, encoder_out, decoder_hidden):
+        '''
+        Forward Pass
+        :param encoder_out: tensor of encoded images (batch, n_pixels, encoder_dim)
+        :param decoder_hidden: tensor of previous decoder output, hidden state (batch, decoder_dim)
+        :return: weighted encoding (from attention), weights
+        '''
+        att1 = self.encoder_atten(encoder_out) #(batch, n_pixels, atten_dim)
+        att2 = self.decoder_atten(decoder_hidden) #(batch, atten_dim)
+        att = self.full_atten(self.relu(att1 + att2.unsqueeze(1))).squeeze(2) #(batch, n_pixels)
+        alpha = self.sfotmax(att) #(batch, n_pixels)
+        atten_weighted_encoding = (encoder_out * alpha.unsqueeze(2)).sum(dim = 1) #(batch, encoder_dim)
+        
+        return atten_weighted_encoding, alpha
+    
+class DecoderWithAttention(nn.Module):
+    
+    def __init__(self, attention_dim, embed_dim, decoder_dim, vocab_size, encoder_dim = 2048, dropout = 0.5):
+       '''
+       :param attention_dim: size of attnetion network
+       :param embed_dim: size of embedding
+       :param decoder_dim: size of RNN decoder
+       :param vocab_size: number of words
+       :param encoder_dim: size of encoded images
+       :param dropout: dropout %
+       '''
+       super(DecoderWithAttention, self).__init__()
+       
+       self.encoder_dim = encoder_dim
+       self.embed_dim = embed_dim
+       self.decoder_dim = decoder_dim
+       self.vocab_size = vocab_size
+       self.dropout = dropout
+       
+       self.attention = Attention(encoder_dim, decoder_dim, attention_dim)
+       
+       self.embedding = nn.Embedding(self.vocab_size, self.embed_dim)
+       self.dropout = nn.Dropout(p = self.dropout)
+       self.decode_step = nn.LSTMCell(embed_dim + encoder_dim, decoder_dim, bias = True) #LSTM decoder
+       self.init_h = nn.Linear(encoder_dim, decoder_dim) #inital hidden state of LSTM
+       self.init_c = nn.Linear(encoder_dim, decoder_dim) #initial cell state of LSTM
+       self.f_beta = nn.Linear(decoder_dim, encoder_dim) #create a sigmoid-activated gate
+       self.sigmoid = nn.Sigmoid()
+       self.fc = nn.Linear(decoder_dim, vocab_size) # identify attention scores of vocab
+       self.init_weights() #initalize layers with uniform distribution
+       
+    def init_weights(self):
+        '''
+        initialize some layers with unifrom stirbution for faster covergence
+        '''
+        self.embedding.weight.data.uniform_(-0.1, 0.1)
+        self.fc.bias.data.fill_(0)
+        self.fc.weight.data.uniform_(-0.1, 0.1)
+        
+    def fine_tune_embeddings(self, fine_tune = True):
+        '''
+        Allow fine tuning of embedding layer (for pretrained embeddings)
+        '''
+        for p in self.embedding.parameters():
+            p.requires_grad = fine_tune
+            
+    def load_pretrained_embeddings(self, embeddings):
+        """
+        Loads embedding layer with pre-trained embeddings.
+        :param embeddings: pre-trained embeddings
+        """
+        self.embedding.weight = nn.Parameter(embeddings)
+        
+    def init_hidden_state(self, encoder_out):
+        '''
+        Initalize hidden state and cell state for LSTM decoder
+        
+        :param encoder_out: tensor of encoded images (batch, n_pixels, encoder_dim)
+        :returns: hidden state, cell state
+        '''
+        mean_encoder_out = encoder_out.mean(dim = 1) #mean across all pixels
+        h = self.init_h(mean_encoder_out) #(batch, decoder_dim)
+        c = self.init_c(mean_encoder_out) #(batch, decoder_dim)
+        return h, c
+    
+    def forward(self, encoder_out, encoded_captions, caption_lengths):
+        '''
+        Forward pass
+        
+        :param encoder_out: tensor of encoded images (batch, enc_img_size, enc_img_size, encoder_dim)
+        :param encoded_captions: tensor of encoded captions (batch, max_caption_length)
+        :param caption_lengths: tensor of size (batch, 1)
+        :return: scores for vocab, sorted captions (encoded), decode lengths, weights, sort indicies
+        '''
+        
+        batch_size = encoder_out.size(0)
+        encoder_dim = encoder_out.size(-1)
+        vocab_size = self.vocab_size
+        
+        encoder_out = encoder_out.view(batch_size, -1, encoder_dim) #(batch_size, n_pixels, encoder_dim)
+        n_pixels = encoder_out.size(1)
+        
+        caption_lengths, sort_ind = caption_lengths.squeeze(1).sort(dim = 0, descending = True)
+        encoder_out = encoder_out[sort_ind]
+        encoded_captions = encoded_captions[sort_ind]
+        
+        embeddings = self.embedding(encoded_captions)
+        
+        h, c = self.init_hidden_state(encoder_out) #(batch, decoder_dim)
+        
+        decode_lengths = (caption_lengths - 1).tolist() #No need to decode <end>
+        
+        #initialize scores and alphas
+        predictions = torch.zeros(batch_size, max(decode_lengths), vocab_size).to(device)
+        alphas = torch.zeros(batch_size, max(decode_lengths), n_pixels).to(device)
+        
+        for t in range(max(decode_lengths)):
+            batch_size_t = sum([l > t for l in decode_lengths])
+            atten_weight_encoding, alpha = self.attention(encoder_out[:batch_size_t], h[:batch_size_t])
+            
+            gate = self.sigmoid(self.f_beta(h[:batch_size_t])) #gating scalar (batch_size_t, encoder_dim)
+            atten_weight_encoding = gate * atten_weight_encoding
+            h, c = self.decode_step(torch.cat([embeddings[:batch_size_t, t, :], atten_weight_encoding], dim = 1),
+                                    (h[:batch_size_t], c[:batch_size_t])) #(batch_t, decoder_dim)
+            
+            preds = self.fc(self.dropout(h)) #(batch_t, vocab)
+            predictions[:batch_size_t, t, :] = preds
+            alphas[:batch_size_t, t, :] = alpha
+            
+        return predictions, encoded_captions, decode_lengths, alphas, sort_ind
---
+++ image_caption/train.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 15:16:28 2019
+
+@author: alec.delany
+"""
+
+    
+import time
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torchvision.transforms as transforms
+from torch import nn
+from torch.nn.utils.rnn import pack_padded_sequence
+from models import Encoder, DecoderWithAttention
+from dataset import CaptionDataset
+from utils import *
+from nltk.translate.bleu_score import corpus_bleu
+from tqdm import tqdm
+import os
+import json
+
+
+
+path = 'input_data/flickr30k_images'
+base_filename = 'flickr30k_4_cap_per_img_5_min_word_freq'
+
+emb_dim = 512
+atten_dim = 512
+decoder_dim = 512
+dropout = 0.5
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+start_epoch = 0
+epochs = 120
+epochs_since_improvment = 0
+batch_size = 32
+workers = 1
+encoder_lr = 1e-4
+decoder_lr = 4e-4
+grad_clip = 5.
+alpha_c = 1. #reg parameter for "doubly stochastic attention"
+best_bleu4 = 0.
+print_freq = 100
+fine_tune_encoder = False
+checkpoint = None
+
+
+def train(train_loader, encoder, decoder, criterion, encoder_optimizer, decoder_optimizer, epoch):
+    '''
+    Trains for one epoch
+    '''
+    print('====* Training for Epoch %i *====' % epoch)
+    
+    decoder.train()
+    encoder.train()
+    
+    losses = AverageMeter()
+        
+    for i, (img, caps, caplens) in enumerate(tqdm(train_loader)):
+        
+        img = img.to(device)
+        caps = caps.to(device)
+        caplens = caplens.to(device)
+        
+        img = encoder(img)
+        scores, caps_sorted, decode_lengths, alphas, sort_ind = decoder(img, caps, caplens)
+        
+        targets = caps_sorted[:, 1:]
+        
+        scores, _ = pack_padded_sequence(scores, decode_lengths, batch_first = True)
+        targets, _ = pack_padded_sequence(targets, decode_lengths, batch_first = True)
+
+        loss = criterion(scores, targets)
+        
+        #Doubly stochastic attention reg
+        loss += alpha_c * ((1. - alphas.sum(dim = 1)) ** 2).mean()   #MSE
+        
+        #back Prop
+        decoder_optimizer.zero_grad()
+        if encoder_optimizer is not None:
+            encoder_optimizer.zero_grad()
+        loss.backward()
+        
+        if grad_clip is not None:
+            clip_gradient(decoder_optimizer, grad_clip)
+            if encoder_optimizer is not None:
+                clip_gradient(encoder_optimizer, grad_clip)
+        
+        decoder_optimizer.step()
+        if encoder_optimizer is not None:
+            encoder_optimizer.step()
+            
+        losses.update(loss.item(), sum(decode_lengths))
+        
+    print('Epoch: ', epoch)
+    print('Loss_val: ', losses.val)
+    print('Loss_avg: ', losses.avg)
+    
+def validate(val_loader, encoder, decoder, criterion):
+    '''
+    Validates one epoch
+    '''
+    
+    decoder.eval()
+    if encoder is not None:
+        encoder.eval()
+        
+    losses = AverageMeter()
+    
+    actuals = [] #true captions
+    yhat = [] #predicted captions
+    
+    with torch.no_grad():
+        for i, (img, caps, caplens, allcaps) in enumerate(tqdm(val_loader)):
+            
+            img = img.to(device)
+            caps = caps.to(device)
+            caplens = caplens.to(device)
+            
+            if encoder is not None:
+                img = encoder(img)
+            scores, caps_sorted, decode_lengths, alphas, sort_ind = decoder(img, caps, caplens)
+            
+            targets = caps_sorted[:, 1:]
+        
+            scores, _ = pack_padded_sequence(scores, decode_lengths, batch_first = True)
+            targets, _ = pack_padded_sequence(targets, decode_lengths, batch_first = True)
+            
+            losses.update(loss.item(), sum(decode_lengths))
+        
+            #print('Loss_val: ', losses.val)
+            #print('Loss_avg: ', losses.avg)
+            
+            #Collecrt actuals
+            allcaps = allcaps[sort_ind]
+            for j in range(allcaps.shape[0]):
+                img_caps = allcaps[j].tolist()
+                img_captions = list(map(lambda x: [w for w in x if w not in {words2idx['<start>'], words2idx['<pad>']}], 
+                                        img_caps))
+                actuals.append(img_captions)
+                
+            #Collect yhat
+            _, preds = torch.max(scores_copy, dim = 2)
+            preds = preds.to_list()
+            temp_preds = list()
+            for j, p in enumerate(preds):
+                temp_preds.append(preds[j][:decode_lengths[j]])
+            preds = temp_preds
+            yhat.extend(preds)
+            
+            assert len(actuals) == len(yhat)
+            
+        bleu4 = corpus_bleu(actuals, yhat)
+        
+        print('Loss_val: ', losses.val)
+        print('Loss_avg: ', losses.avg) 
+        
+    return bleu4
+ 
+
+with open(os.path.join(path, 'WORDMAP_' + base_filename + '.json'), 'r') as j:
+    words2idx = json.load(j)
+
+if checkpoint is None:
+    decoder = DecoderWithAttention(attention_dim = atten_dim, embed_dim = emb_dim, decoder_dim = decoder_dim,
+                                   vocab_size = len(words2idx), dropout = dropout)
+    decoder_optimizer = torch.optim.Adam(params = filter(lambda p: p.requires_grad, decoder.parameters()), lr = decoder_lr)
+    
+    encoder = Encoder()
+    encoder.fine_tune(fine_tune_encoder)
+    if fine_tune_encoder:
+        encoder_optimizer = torch.optim.Adam(params = filter(lambda p: p.requires_grad, encoder.parameters()), 
+                                             lr = encoder_lr)
+    else:
+        encoder_optimizer = None
+        
+else:
+    checkpoint = torch.load(checkpoint)
+    start_epoch = checkpoint['epoch'] + 1
+    epochs_since_improvement = checkpoint['epochs_since_improvement']
+    best_bleu4 = checkpoint['bleu-4']
+    decoder = checkpoint['decoder']
+    decoder_optimizer = checkpoint['decoder_optimizer']
+    encoder = checkpoint['encoder']
+    encoder_optimizer = checkpoint['encoder_optimizer']
+    
+    if fine_tune_encoder is True and encoder_optimizer is None:
+        encoder.fine_tune(fine_tune_encoder)
+        encoder_optimizer = torch.optim.Adam(params = filter(lambda p: p.requires_grad, encoder.parameters()), lr = encoder_lr)
+
+decoder = decoder.to(device)
+encoder = encoder.to(device)
+
+criterion = nn.CrossEntropyLoss().to(device)
+
+normalize = transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.255])
+
+train_loader = torch.utils.data.DataLoader(CaptionDataset(path, base_filename, 'TRAIN', transform = transforms.Compase([normalize])),
+                                          batch_size = batch_size, shuffle = True, num_workers = workers, pin_memory = True)
+val_loader = torch.utils.data.DataLoader(CaptionDataset(path, base_filename, 'VAL', transform = transforms.Compase([normalize])),
+                                          batch_size = batch_size, shuffle = True, num_workers = workers, pin_memory = True)
+
+for epoch in range(start_epoch, epochs):
+    #early stopping if no imporvement after 20 epochs
+    if epochs_since_improvement == 20:
+        break
+    if (epochs_since_improvement > 0) and (epochs_since_improvement % 8 == 0):
+        adjust_learning_rate(decoder_optimizer, 0.8)
+        if fine_tune_encoder:
+            adjust_learning_rate(encoder_optimizer, 0.8)
+            
+    train(train_loader = train_loader, encoder = encoder, decoder = decoder, crieterion = criterion, 
+                          encoder_optimizer = encoder_optimizer, decoder_optimizer = decoder_optimizer, epoch = epoch)
+    
+    recent_bleu4 = validate(val_loader = val_loader, encoder = encoder, decoder = decoder, criterion = criterion)
+    
+    is_best = recent_bleu4 > best_bleu4
+    best_bleu4 = max(recent_bleu4, best_bleu4)
+    if not is_best:
+        epochs_since_improvement += 1
+        print ('Epochs since imrpovements: ', epochs_since_improvement)
+    else:
+        epochs_since_improvement = 0
+    
+    save_checkpoint(data_name, epoch, epochs_since_improvement, encoder, decoder, encoder_optimizer,
+                    decoder_optimizer, recent_bleu4, is_best)
